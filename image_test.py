cf3ccfa7c6f92710e0df78a625b1fad629363247
Het Suthar <hetsthr@gmail.com>
2022-10-16 17:06:01 +0530

    modified to run with webcam video stream

--- image_test.py
+++ image_test.py
@@ -4,10 +4,17 @@ import numpy as np
 import time
 import cv2
 
-def classify_image(interpreter, image, top_k=1):
+def load_labels(path): # Read the labels from the text file as a Python list.
+  with open(path, 'r') as f:
+    return [line.strip() for i, line in enumerate(f.readlines())]
+
+def set_input_tensor(interpreter, image):
   tensor_index = interpreter.get_input_details()[0]['index']
   input_tensor = interpreter.tensor(tensor_index)()[0]
   input_tensor[:, :] = image
+
+def classify_image(interpreter, image, top_k=1):
+  set_input_tensor(interpreter, image)
 
   interpreter.invoke()
   output_details = interpreter.get_output_details()[0]
@@ -16,12 +23,13 @@ def classify_image(interpreter, image, top_k=1):
   scale, zero_point = output_details['quantization']
   output = scale * (output - zero_point)
 
-  ordered = np.argpartition(-output, top_k)
+  ordered = np.argpartition(-output, 1)
   return [(i, output[i]) for i in ordered[:top_k]][0]
 
 model_path = "mobilenet_rgb_32_quant.tflite"
 data_folder = "fire_data/"
 label_path = "labels.txt"
+
 interpreter = Interpreter(model_path)
 print("Model Loaded Successfully.")
 
@@ -29,19 +37,31 @@ interpreter.allocate_tensors()
 _, height, width, _ = interpreter.get_input_details()[0]['shape']
 print("Image Shape (", width, ",", height, ")")
 
-# Load an image to be classified.
-image = Image.open(data_folder + "test/fire.751.png").convert('RGB').resize((width, height))
-# Classify the image.
-time1 = time.time()
-label_id, prob = classify_image(interpreter, image)
-time2 = time.time()
-classification_time = np.round(time2-time1, 3)
-print("Classificaiton Time =", classification_time, "seconds.")
+url = "http://192.168.29.9:8080/video"
 
-# Read class labels.
-labels = load_labels(label_path)
-
-# Return the classification label of the image.
-classification_label = labels[label_id]
-print("Image Label is :", classification_label, ", with Accuracy :", np.round(prob*100, 2), "%.")
+cp = cv2.VideoCapture(url)
+while(True):
+    camera, frame = cp.read()
+    if frame is not None:
+        cv2.imshow("Frame", frame)
+    q = cv2.waitKey(1)
+    # Load an image to be classified.
+    image = Image.fromarray(frame).convert('RGB').resize((width, height))
+    
+    # Classify the image.
+    time1 = time.time()
+    label_id, prob = classify_image(interpreter, image)
+    time2 = time.time()
+    classification_time = np.round(time2-time1, 3)
+    print("Classificaiton Time =", classification_time, "seconds.")
+    
+    # Read class labels.
+    labels = load_labels(label_path)
+    
+    # Return the classification label of the image.
+    classification_label = labels[label_id]
+    print("Image Label is :", classification_label, ", with Accuracy :", np.round(prob*100, 2), "%.")
+    if q==ord("q"):
+        break
+cv2.destroyAllWindows()
 
